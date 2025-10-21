2d796ffa520d3ddd23797a248aa971f445ff13dd
Abhinav Mathew <amonline2005@gmail.com>
2025-10-21 15:01:27 +0530

    popupmade

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import TopBar from "@/components/topBar";
 import LandingSection from "@/components/landingSection";
 import Footer from "@/components/footer";
+import FomoAIpopup from "@/components/FomoAIpopup";
 
 export default function Home() {
   return (
     <>
-      <TopBar />
+      <TopBar theme="black" />
       <LandingSection />
       <h1 className="flex items-center justify-center h-[80vh] bg-red-500 text-center text-5xl">
         section 1
@@ -16,6 +17,7 @@ export default function Home() {
       <h1 className="flex items-center justify-center h-[80vh] bg-amber-500 text-center text-5xl">
         section 3
       </h1>
+
       <Footer />
     </>
   );
