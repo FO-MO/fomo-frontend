366da3711433518810bf69d8d2d21b883e6d0bcb
Abhinav Mathew <amonline2005@gmail.com>
2025-10-26 17:13:46 +0530

    hero-progress made

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import TopBar from "@/components/topBar";
 import LandingSection from "@/components/landingSection";
 import Footer from "@/components/footer";
+import Hero1 from "@/components/hero/Hero1";
 
 import Link from "next/link";
 
@@ -9,15 +10,7 @@ export default function Home() {
     <>
       <TopBar theme="home" />
       <LandingSection />
-      <div className="flex flex-col items-center justify-center h-[80vh] bg-red-500 text-center text-white">
-        <h1 className="text-5xl font-bold mb-8">For Students</h1>
-        <Link
-          href="/students/search"
-          className="bg-white text-red-500 px-8 py-4 rounded-lg text-xl font-semibold hover:bg-gray-100 transition-colors"
-        >
-          Explore Student Portal
-        </Link>
-      </div>
+      <Hero1 />
       <div className="flex flex-col items-center justify-center h-[80vh] bg-indigo-900 text-center text-white">
         <h1 className="text-5xl font-bold mb-8">For Colleges</h1>
         <Link
